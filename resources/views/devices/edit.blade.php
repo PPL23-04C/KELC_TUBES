e6fa7a1a060e46cb8453f4a8434f95c93f627198
Nivek-IP <ignatius.kevinm@gmail.com>
2026-04-21 21:25:57 +0700

    DEV-47 Membuat Fitur Kelola Data Elektronik

---
+++ resources/views/devices/edit.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('title', 'Ubah Perangkat')
+@section('page-title', 'Ubah Perangkat')
+
+@section('content')
+    <div class="form-card">
+        <form method="POST" action="{{ route('devices.update', $device) }}">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="nama_device">Nama Perangkat</label>
+                <input id="nama_device" name="nama_device" value="{{ old('nama_device', $device->nama_device) }}" required>
+            </div>
+            <div class="form-group">
+                <label for="daya_watt">Daya (Watt)</label>
+                <input id="daya_watt" type="number" name="daya_watt" min="1" value="{{ old('daya_watt', $device->daya_watt) }}" required>
+            </div>
+            <div class="form-group">
+                <label for="jumlah_unit">Jumlah Unit</label>
+                <input id="jumlah_unit" type="number" name="jumlah_unit" min="1" value="{{ old('jumlah_unit', $device->jumlah_unit) }}" required>
+            </div>
+            <button class="btn" type="submit">Simpan</button>
+            <a class="btn secondary" href="{{ route('devices.index') }}">Batal</a>
+        </form>
+    </div>
+@endsection
