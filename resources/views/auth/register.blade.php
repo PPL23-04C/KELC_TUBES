54102f944907222a2cc4af08b3d5940dbddf7379
zhahere <marza1824@gmail.com>
2026-04-29 10:51:55 +0700

    DEV-103 Menambahkan fitur input daya VA

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -32,7 +32,16 @@
                     <label for="email">Email</label>
                     <input id="email" type="email" name="email" value="{{ old('email') }}" required>
                 </div>
-
+                <div class="form-group">
+                    <label for="daya_va">Daya Listrik (VA)</label>
+                    <select id="daya_va" name="daya_va" required>
+                        <option value="450">450 VA</option>
+                        <option value="900">900 VA</option>
+                        <option value="1300" selected>1300 VA</option>
+                        <option value="2200">2200 VA</option>
+                        <option value="3500">3500 VA+</option>
+                    </select>
+                </div>
                 <div class="form-group">
                     <label for="password">Password</label>
                     <input id="password" type="password" name="password" required>
