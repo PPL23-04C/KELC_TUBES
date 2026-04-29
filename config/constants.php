b52486a2defa84384edd25501d341fe2f59306fa
Nivek-IP <ignatius.kevinm@gmail.com>
2026-04-29 11:17:30 +0700

    DEV-106 Membuat fungsi tarif listrik

--- config/constants.php
+++ config/constants.php
@@ -1,8 +1,16 @@
 <?php
 
 return [
+    'tariffs' => [
+        450 => 415,
+        900 => 1352,
+        1300 => 1444.70,
+        2200 => 1444.70,
+        3500 => 1699.53,
+        'default' => 1444.70,
+    ],
 
-    
+
     'daily_alert_messages' => [
         'boros' => 'PERINGATAN! Listrik BOROS: {pemakaian} kWh (batas {batas} kWh)',
         'sedang' => 'Penggunaan listrik hari ini sedang: {pemakaian} kWh.',
