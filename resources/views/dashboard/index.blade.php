d157a779821a9a78725ff122e73752a1c7c4828c
Lia <helialia342@gmail.com>
2026-04-30 00:26:00 +0700

    DEV-108 Membuat grafik penggunaan listrik fixed

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -19,12 +19,6 @@
             <h3>Estimasi Biaya/bulan</h3>
             <div class="value">Rp {{ number_format($totalCost, 2, ',', '.') }}</div>
         </div>
-        <div class="card">
-            <div class="chip">Emisi</div>
-            <h3>Emisi CO2/bulan</h3>
-            <div class="value">{{ number_format($totalCo2, 2) }} kg</div>
-        </div>
-    </div>
 
     <div class="grid grid-2" style="margin-top: 20px;">
         <div class="card">
@@ -46,15 +40,6 @@
     <div class="card" style="margin-top: 24px;">
         <h3>Grafik Penggunaan (7 Hari)</h3>
         <canvas id="usageChart" height="120"></canvas>
-    </div>
-
-    <div class="card" style="margin-top: 24px;">
-        <h3>Tips Hemat Energi</h3>
-        <ul style="margin: 0; padding-left: 18px; color: var(--muted);">
-            @foreach($tips as $tip)
-                <li>{{ $tip }}</li>
-            @endforeach
-        </ul>
     </div>
 @endsection
 
