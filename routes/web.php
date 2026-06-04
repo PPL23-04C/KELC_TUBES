5349c183280518de6351400c605ea4c85cebdaf3
Nivek-IP <ignatius.kevinm@gmail.com>
2026-06-04 22:29:24 +0700

    DEV-135 Membuat Fitur Berita SDG 7

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,15 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-    
+
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('devices', DeviceController::class)->except(['show']);
     Route::get('/history', [HistoryController::class, 'index'])->name('history.index');
     Route::get('/recommendations', [RecommendationController::class, 'index'])->name('recommendations.index');
     Route::get('/leaderboard', [LeaderboardController::class, 'index'])->name('leaderboards.index');
+
+    Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+    Route::get('/news/{id}', [NewsController::class, 'show'])->name('news.show');
 });
 
 Route::get('/', function () {
