8d1f33c229016a8877178be358970cae8ed2c7d2
satriafazal <satriafaza24@gmail.com>
2026-06-15 00:21:09 +0700

    DEV-206 improve tampilan reminder

--- app/Http/Controllers/RecommendationController.php
+++ app/Http/Controllers/RecommendationController.php
@@ -569,27 +569,4 @@ class RecommendationController extends Controller
 
         return $tips;
     }
-
-    public function toggleTipChecklist(Request $request): JsonResponse
-    {
-        $user = auth()->user();
-
-        $validated = $request->validate([
-            'device_id' => 'required|integer',
-            'tip_index' => 'required|integer|min:0',
-        ]);
-
-        $sessionKey = "recommendation_tip_checklists.user_{$user->id}";
-        $tipChecklists = session()->get($sessionKey, []);
-        $checklistKey = "{$validated['device_id']}_{$validated['tip_index']}";
-        $isCompleted = !($tipChecklists[$checklistKey] ?? false);
-
-        $tipChecklists[$checklistKey] = $isCompleted;
-        session()->put($sessionKey, $tipChecklists);
-
-        return response()->json([
-            'success' => true,
-            'is_completed' => $isCompleted,
-        ]);
-    }
 }
